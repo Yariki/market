5eee64ac7718c6e7d9772fa03f89c7f5894f6ad7
Yaroslav Lisovskyi <iyariki.ya@gmail.com>
2023-09-06 23:34:40 +0300

    initial adding controller for catalogs

---
+++ src/ProductCatalog/API/Controllers/CatalogController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductCatalog.Application.Catalogs.Commands.AddCatalog;
+
+namespace ProductCatalog.WebUI.Controllers;
+
+public class CatalogController : ApiControllerBase
+{
+
+    [HttpPost()]
+    public async Task<ActionResult<Guid>> AddCatalog(AddCatalogCommand addCatalogCommand)
+    {
+        var guid = await Mediator.Send(addCatalogCommand);
+
+        return Ok(guid);
+    }
+    
+}
