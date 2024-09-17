eee5a8a2e14ae0d4f6fb1f5b2f17792b6e5225d3
Yariki <iyariki.ya@gmail.com>
2024-09-17 22:52:37 +0300

    remove Orders old project

--- src/Orders/src/Application/Common/Exceptions/ForbiddenAccessException.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Orders.Application.Common.Exceptions;
-
-public class ForbiddenAccessException : Exception
-{
-    public ForbiddenAccessException() : base() { }
-}
--- src/Orders/src/Domain/Exceptions/UnsupportedSampleValueException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Orders.Domain.Exceptions;
-
-public class UnsupportedSampleValueException : Exception
-{
-    public UnsupportedSampleValueException(string code)
-        : base($"Sample value \"{code}\" is unsupported.")
-    {
-    }
-}
